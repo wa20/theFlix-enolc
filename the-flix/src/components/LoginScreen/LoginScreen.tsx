b290ff8c63098f83b99585bc23f58fd6df5c003b
wael <78425538+wa20@users.noreply.github.com>
2026-06-07 23:42:42 +0100

    login page creation

--- the-flix/src/components/LoginScreen/LoginScreen.tsx
+++ the-flix/src/components/LoginScreen/LoginScreen.tsx
@@ -1,5 +1,7 @@
 import {useState} from 'react'
 import styles from './styles.module.css'
+import { sign } from 'node:crypto';
+import SignUpModal from '../SignUpModal/SignUpModal';
 
 function LoginScreen() {
  const [signIn, setSignIn] = useState(false);
@@ -11,19 +13,20 @@ function LoginScreen() {
         <div className={styles.loginScreenContainer}>
             <img 
                 className={styles.loginScreenLogo}
-                // src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt=""
-                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo"  
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Background"  
                 />
                 <button 
                 className={styles.loginScreenButton}
                 onClick={() => setSignIn(true)}>
                 Sign In
                 </button>
-
                 <div className={styles.loginScreenGradient}></div>
         </div>
 
         <div className={styles.loginScreenBody}>
+            {signIn ? (
+                <SignUpModal />
+            ) : 
             <>
              <h1>Unlimited films, TV programmes and more.</h1>
              <h2>Watch anywhere. Cancel at any time.</h2>
@@ -39,6 +42,7 @@ function LoginScreen() {
                     </form>
                 </div>
             </>
+            }
         </div>
  
     </div>
