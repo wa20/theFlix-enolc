88d4768dae8af13f2d5c88a9378876eb583818be
wael <78425538+wa20@users.noreply.github.com>
2026-06-07 13:09:41 +0100

    additional react app build set up

---
+++ the-flix/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDXJOMDtevklTJnhk0s4S3gcYsRa4lTsGc",
+  authDomain: "the-flix-enolc.firebaseapp.com",
+  projectId: "the-flix-enolc",
+  storageBucket: "the-flix-enolc.firebasestorage.app",
+  messagingSenderId: "458165915899",
+  appId: "1:458165915899:web:0225e8d9911944e877e05e",
+  measurementId: "G-HM98NC4LN9"
+};
