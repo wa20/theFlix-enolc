b028d77d1fc1effcefdbfa2ba479c11e0c788d14
wael <78425538+wa20@users.noreply.github.com>
2026-06-07 16:38:27 +0100

    navbar styling and build

--- the-flix/src/components/Navbar/Navbar.jsx
+++ the-flix/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 // import React from 'react'
+import { useState, useEffect } from 'react'
 import styles from './styles.module.css'
-
 function Navbar() {
+  const [show, handleShow] = useState(false)
+
+  const transitionNavbar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true)
+    } else {
+      handleShow(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavbar)
+    return () => window.removeEventListener('scroll', transitionNavbar)
+  }, [])
+
   return (
     <> 
-    <div className={styles.navbarContainer}>
+    <div className={`${styles.navbarContainer} ${show && styles.navbarContainerBlack}`}>
        <div className={styles.navbar}>
          <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo" className={styles.logo} />
         <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Classic Blue Avatar" className={styles.avatar} />
