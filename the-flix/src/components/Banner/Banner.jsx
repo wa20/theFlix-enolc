049044cfda28b7ff50c2dea0674414573e0cd334
wael <78425538+wa20@users.noreply.github.com>
2026-06-07 18:32:07 +0100

    more styling amends

--- the-flix/src/components/Banner/Banner.jsx
+++ the-flix/src/components/Banner/Banner.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
 
 import styles from './styles.module.css'
+import { useEffect } from 'react';
+import axios from '../../../axios';
+import { requests } from '../../../Request';
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {    
+        const fetchData = async () => { 
+            const response = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                response.data.results[
+                    Math.floor(Math.random() * response.data.results.length - 1)
+                ]
+            );
+            return response;
+        }
+        fetchData();
+    }, [])
+
+    console.log({movie}) // For debugging purposes, you can remove this line in production.
 
    
   const truncate = (str, n) => {
@@ -17,18 +36,18 @@ function Banner() {
     style={{
         backgroundSize: 'cover', 
         backgroundPosition: 'center center',
-        backgroundImage: `url(https://image.tmdb.org/t/p/original/b7U9sE96HOsd8oV1BQeWhja2EXs.jpg)` 
-        //  backgroundImage: `url(http://image.tmdb.org/t/p/original/cjSB3ZjaMpaa9oHFyeYha3KG0mp.jpg)` 
+        // backgroundImage: `url(https://image.tmdb.org/t/p/original/b7U9sE96HOsd8oV1BQeWhja2EXs.jpg)`, 
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`, 
     }}>
         <div className={styles.bannerContents}>
-            <h1 className={styles.bannerTitle}>The Flix List</h1>
+            <h1 className={styles.bannerTitle}>{movie?.title || movie?.name || movie?.original_name}</h1>
             <div className={styles.bannerButtons}>
                 <button className={styles.playButton}>Play</button>
                 <button className={styles.myListButton}>My List</button>
             </div>
-            <h1 className={styles.bannerDescription}>
-                {truncate("Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.", 100)}
-                </h1>
+            <div className={styles.bannerDescription}>
+                {truncate(movie?.overview, 120)}
+            </div>
         </div>
         <div className={styles.bannerFadeBottom} />
 
