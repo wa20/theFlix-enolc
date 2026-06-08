cee60fb455180a07e06f688a6a6623dc3675f0d7
wael <78425538+wa20@users.noreply.github.com>
2026-06-08 23:25:22 +0100

    updated app and userSlice

--- the-flix/src/App.jsx
+++ the-flix/src/App.jsx
@@ -1,22 +1,52 @@
 // import { useState } from 'react'
-import HomeScreen from './components/HomeScreen/HomeScreen'
-import LoginScreen from './components/LoginScreen/LoginScreen'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from "react";
+import HomeScreen from "./components/HomeScreen/HomeScreen";
+import LoginScreen from "./components/LoginScreen/LoginScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { authUser } from "../firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = authUser.onAuthStateChanged((user) => {
+      if (!user) {
+        dispatch(logout);
+      }
+
+      dispatch(
+        login({
+          uid: user.uid,
+          email: user.email,
+        }),
+      );
+
+      console.log("§ User logged in", { user });
+      // Add logged in logic
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <>
       <section id="center">
         <Router>
-          <Routes>
-            <Route exact path="/" element={<HomeScreen />} />
-            <Route path="/login" element={<LoginScreen />} />
-          </Routes>
+          {!user ? (
+            <LoginScreen />
+          ) : (
+            <Routes>
+              <Route exact path="/" element={<HomeScreen />} />
+              <Route path="/login" element={<LoginScreen />} />
+            </Routes>
+          )}
         </Router>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- the-flix/src/features/userSlice.js
+++ the-flix/src/features/userSlice.js
@@ -17,4 +17,9 @@ const userSlice = createSlice({
 });
 
 export const { login, logout } = userSlice.actions;
+
+
+export const selectUser = state => state.user.user;
+
+
 export default userSlice.reducer;
