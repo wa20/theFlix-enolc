c6c47daeb634c3a4e09d0e7090a47ec685938df1
wael <78425538+wa20@users.noreply.github.com>
2026-06-08 22:06:44 +0100

    updated firebase config, and added auth checks to signup component

--- the-flix/firebase.js
+++ the-flix/firebase.js
@@ -1,6 +1,8 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +18,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const app = firebase.initializeApp(firebaseConfig);
+const db = app.firestore()
+const authUser = firebase.auth()
 
-export default firebaseConfig;
+
+export { authUser }
+export default db;
--- the-flix/src/components/SignUpModal/SignUpModal.jsx
+++ the-flix/src/components/SignUpModal/SignUpModal.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
+import {useRef} from 'react'
+
 import styles from './styles.module.css'
+import { authUser } from '../../../firebase'
 
 function SignUpModal() {
+
+  const emailRef = useRef()
+  const passwordRef = useRef()
 
   const register = (e) => {
     e.preventDefault();
 
+    authUser.createUserWithEmailAndPassword(
+      emailRef.current.value,
+      passwordRef.current.value
+    ).then((auth) =>{
+        console.log('§ sign up', {auth})
+    }).catch(error => {
+      alert(error)
+    })
   }
 
   const signIn = (e) => { 
     e.preventDefault()
 
+    authUser.signInWithEmailAndPassword(
+      emailRef.current.value,
+      passwordRef.current.value
+    ).then( auth => {
+      console.log('§ sign in', {auth})
+    }).catch(error => {
+      alert(error)
+    })
   }
 
   return (
     <div className={styles.signUpModal}>
        <form>
          <h1>Sign In</h1>
-         <input placeholder='Email' type='email' />
-         <input placeholder='Password' type='password'/>
+         <input ref={emailRef} placeholder='Email' type='email' />
+         <input ref={passwordRef} placeholder='Password' type='password'/>
          <button 
           onClick={signIn}
           type='submit'>
